ad9de110ff56f6fb493a53b0a211f310e20e66ce
Mmarmedina <maria.martin.medina.84@gmail.com>
2024-01-10 12:08:59 +0100

    Tooltips formulario

--- Video-Games-Library-React-and-Bootstrap/src/components/FormNewGame/FormNewGame.tsx
+++ Video-Games-Library-React-and-Bootstrap/src/components/FormNewGame/FormNewGame.tsx
@@ -21,11 +21,23 @@ function FormNewGame () {
         publisher: '',
         price: '',
         img: ''
-    })
+    });
 
-    function handleOnSubmit (event: any) {
-        event.preventDefault()
-        console.log ('está funcionando onsubmit')
+
+    const [validated, setValidated] = useState(false); 
+    
+    const [isInvalid, setIsInvalid] = useState(false)
+    
+
+    function handleOnSubmit (event: any) {   
+
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            setIsInvalid(!isInvalid)
+        }
+        setValidated(true);
     }
 
     function handleInputChange (event: any) {
@@ -33,6 +45,7 @@ function FormNewGame () {
         event.preventDefault()
 
         setInputValues( { ...inputValues, [event.target.name]: event.target.value})
+
         console.log (inputValues)       
 
         console.log ('Está esta guardando el form') 
@@ -46,23 +59,28 @@ function FormNewGame () {
                 <h1 className={styles.title}>Añade un nuevo videojuego a tu biblioteca</h1>
             </header>            
             <main>
-                <Form onSubmit={handleOnSubmit}>         
+                <Form noValidate validated={validated} onSubmit={handleOnSubmit}>         
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="title">
-                            <Form.Label column="sm" lg={5} className="fw-bold mb-1">Título del vídeojuego</Form.Label>
-                            <Form.Control 
-                                type="text" 
+                            <Form.Label column="sm" lg={5} className={styles.formLabel}>Título del vídeojuego</Form.Label>
+                            <Form.Control                                
+                                required
+                                type="text"
+                                size="sm" 
                                 placeholder="Escribe el título del vídeojuego"
                                 name="title"
                                 value={inputValues.title}
                                 onChange={(e) => handleInputChange(e)}
-                            />
+                            />                            
                         </Form.Group>
+                        { isInvalid && <span>El campo es obligatorio</span> }
                     </Row>
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="releaseDate">
-                            <Form.Label column="sm" lg={5} className="fw-bold mb-1">Fecha lanzamiento</Form.Label>
-                            <Form.Control 
+                            <Form.Label column="sm" lg={5} className={styles.formLabel}>Fecha lanzamiento</Form.Label>
+                            <Form.Control
+                                required
+                                size="sm"
                                 type="date"
                                 name="releaseDate"
                                 value={inputValues.releaseDate}
@@ -70,8 +88,10 @@ function FormNewGame () {
                             />
                         </Form.Group>
                         <Form.Group as={Col} controlId="price" className="w-25"> 
-                            <Form.Label column="sm" lg={5} className="fw-bold mb-1">Precio (€)</Form.Label>
-                            <Form.Control 
+                            <Form.Label column="sm" lg={5} className={styles.formLabel}>Precio (€)</Form.Label>
+                            <Form.Control
+                                required
+                                size="sm" 
                                 type="number" 
                                 placeholder="Precio"
                                 name="price"
@@ -85,8 +105,10 @@ function FormNewGame () {
                     </Row>
                     <Row className="mb-3 w-50">
                         <Form.Group as={Col} controlId="pegi">
-                            <Form.Label column="sm" lg={5} className="fw-bold mb-1">Edad recomendada</Form.Label>
+                            <Form.Label column="sm" lg={5} className={styles.formLabel}>Edad recomendada</Form.Label>
                             <Form.Select defaultValue="Selecciona..."
+                                required
+                                size="sm"
                                 name="pegi"
                                 value={inputValues.pegi}
                                 onChange={(e) => handleInputChange(e)}                    
@@ -103,12 +125,15 @@ function FormNewGame () {
 
                     <Row className="mb-3">                                
                         <Form.Group as={Col} controlId="genre">
-                            <Form.Label column="sm" lg={5} className="fw-bold mb-1">Género</Form.Label>
+                            <Form.Label column="sm" lg={5} className={styles.formLabel}>Género</Form.Label>
                             <Form.Select defaultValue="Selecciona..."
+                                required
+                                size="sm"
                                 name="genre"
                                 value={inputValues.genre}
                                 onChange={(e) => handleInputChange(e)}                
-                            >                     
+                            >    
+                                 <option>Selecciona el género</option>                 
                                 <option value="Acción">Acción</option>
                                 <option value="Aventura">Aventura</option>
                                 <option value="Estrategia">Estrategia</option>
@@ -118,8 +143,10 @@ function FormNewGame () {
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="publisher" required>
-                            <Form.Label column="sm" lg={5} className="fw-bold mb-1">Autor</Form.Label>
+                            <Form.Label column="sm" lg={5} className={styles.formLabel}>Autor</Form.Label>
                             <Form.Select defaultValue="Selecciona..."
+                                required
+                                size="sm"
                                 name="publisher"
                                 value={inputValues.publisher}
                                 onChange={(e) => handleInputChange(e)}                    
@@ -134,9 +161,11 @@ function FormNewGame () {
                     </Row>
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="img">
-                            <Form.Label column="sm" lg={5} className="fw-bold mb-1">Imagen</Form.Label>
+                            <Form.Label column="sm" lg={5} className={styles.formLabel}>Imagen</Form.Label>
                             <Form.Control 
-                                type="img" 
+                                required
+                                size="sm"
+                                type="url" 
                                 placeholder="URL imagen del vídeojuego"
                                 name="img"
                                 value={inputValues.img}
